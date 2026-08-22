fc97b576f944bcfa61ca331a7ddbba723de8d757
giulianopz <45801466+giulianopz@users.noreply.github.com>
2026-08-22 11:44:38 +0000

    Deploying to gh-pages from @ giulianopz/programming@065b371640cdb498c8c108d009b465a8e5093e80 🚀

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c58d67a8.js',
+                path_to_root + 'searchindex-64d45a44.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
